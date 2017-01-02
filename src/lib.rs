03fa113984fe612e6f7d7b7e4c68ebbd045cf9ae
mvaude <maxime.vaude@scality.com>
2017-01-02 13:34:56 +0100

    ft(base): add main run_cmd function with some unit tests

---
+++ src/lib.rs
@@ -0,0 +1,49 @@
+#[macro_use]
+extern crate slog;
+extern crate slog_term;
+
+use slog::DrainExt;
+use std::process::{Command, Output};
+
+pub fn run_cmd(cmd: &mut Vec<String>) -> Result<Output, std::io::Error> {
+    let log = slog::Logger::root(slog_term::streamer().full().build().fuse(), o!("version" => env!("CARGO_PKG_VERSION")));
+
+    trace!(log, "Starting with Command");
+    info!(log, "Starting with Command: {}", cmd.join(" "));
+    let base = cmd.remove(0);
+    let mut command = Command::new(base);
+    for part in cmd {
+        command.arg(part);
+    }
+    command.output()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::run_cmd;
+    use std::env;
+
+    #[test]
+    fn run_cmd_success_output() {
+        let mut cmd = vec!["pwd".to_string()];
+        let exp = env::var("PWD").unwrap().to_string() + "\n";
+        let res = run_cmd(&mut cmd).unwrap();
+        let stdout = String::from_utf8(res.stdout).unwrap();
+        assert_eq!(exp, stdout);
+    }
+
+    #[test]
+    fn run_cmd_success_return_code() {
+        let mut cmd = vec!["pwd".to_string()];
+        let res = run_cmd(&mut cmd).unwrap();
+        assert_eq!(true, res.status.success());
+    }
+
+    #[test]
+    #[should_panic(expected = "No such file or directory")]
+    fn run_cmd_failure_return_code() {
+        let mut cmd = vec!["blabla".to_string()];
+        let res = run_cmd(&mut cmd);
+        assert_eq!(false, res.unwrap().status.success());
+    }
+}
